0a1445bb1bf250290e8eeb9b3d3c02f21ca7b357
Bit7eg <stonysyrop@gmail.com>
2022-09-08 22:52:33 +0700

    First part done.

--- src/Task1.java
+++ src/Task1.java
@@ -1,55 +1,38 @@
-import java.util.LinkedList;
 import java.util.Map;
-
-class MathFunction {
-    // TODO: think about using this class from other file
-    private String expression;
-    MathFunction() {
-        this.expression = "0";
-    }
-    MathFunction(String expression) throws NullPointerException {
-        // TODO: rewrite this using https://habr.com/ru/post/596925/
-        char ch;
-        Character buff = null;
-        LinkedList<Character> stack = new LinkedList<>();
-        for (int i = 0; i < expression.length(); i++) {
-            ch = expression.charAt(i);
-            if (ch <= '9' && ch >= '0') {
-                this.expression += ch;
-            } else if (ch == '+' || ch == '-') {
-                this.expression += " ";
-                buff = stack.peekFirst();
-                while (buff != null && (buff == '*' || buff == '/')) {
-                    this.expression += stack.pollFirst() + " ";
-                    buff = stack.peekFirst();
-                }
-                stack.addFirst(ch);
-            } else if (ch == '*' || ch == '/') {
-                this.expression += " ";
-                stack.addFirst(ch);
-            } else if (ch == '(') {
-                stack.addFirst(ch);
-            } else if (ch == ')') {
-                this.expression += " ";
-                buff = stack.pollFirst();
-                while (buff != '(') {
-                    this.expression += buff + " ";
-                    buff = stack.pollFirst();
-                }
-            }
-        }
-    }
-    MathFunction(Map<Double, Double> known_pairs) {
-        //TODO: add functional
-    }
-    public double calculate(double x) {
-        //TODO: add calculating
-        return x;
-    }
-}
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Task1 {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args){
-        //TODO: add program logic
+        firstSubtask();
+    }
+    private static void firstSubtask() {
+        System.out.printf("Enter number of known pairs: ");
+        Integer N = in.nextInt();
+        TreeMap<Double, Double> XtoY = new TreeMap<>(Double::compareTo);
+        System.out.printf("\nEnter %d known pairs:\n", N);
+        for (Integer i = 0; i <= N; i++) {
+            XtoY.put(in.nextDouble(), in.nextDouble());
+        }
+        System.out.printf("\nEnter argument to calculate: ");
+        Double x = in.nextDouble();
+        Map.Entry<Double, Double> rPair = null,
+                lPair = null;
+        for (Map.Entry<Double, Double> pair:
+             XtoY.entrySet()) {
+            if (x < pair.getKey()) {
+                rPair = pair;
+                break;
+            }
+        }
+        lPair = XtoY.lowerEntry(rPair.getKey());
+        Double y = (x - lPair.getKey()) / (rPair.getKey() - lPair.getKey()) *
+                (rPair.getValue() - lPair.getValue()) + lPair.getValue();
+        System.out.printf("\nResult: %50.25e", y);
+    }
+    private static void secondSubtask() {
+        // TODO: add program logic
     }
 }
