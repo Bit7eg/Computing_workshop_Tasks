9ddd50e58166445463bfb4de76120cfa584a6742
Bit7eg <stonysyrop@gmail.com>
2022-09-12 17:51:56 +0700

    Done first and second parts of task.

--- src/Task1.java
+++ src/Task1.java
@@ -1,44 +1,139 @@
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class Task1 {
-    static Scanner in = new Scanner(System.in);
+    public static PrintWriter outLines;
+    public static PrintWriter outPolynomial;
+    public static HashMap<Double, Double> XAndDividedDifference;
+    public static TreeMap<Double, Double> XtoY = new TreeMap<>(Double::compareTo);
 
-    public static void main(String[] args){
-        firstSubtask();
-    }
-    private static void firstSubtask() {
-        System.out.printf("Enter number of known pairs: ");
-        Integer N = in.nextInt();
-        TreeMap<Double, Double> XtoY = new TreeMap<>(Double::compareTo);
-        System.out.printf("\nEnter %d known pairs:\n", N);
-        for (Integer i = 0; i <= N; i++) {
-            XtoY.put(in.nextDouble(), in.nextDouble());
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Scanner inFile;
+
+        System.out.print("Enter filepath for...\n" +
+                "\tKnown pairs data: ");
+        String Path = in.nextLine();
+        try {
+            inFile = new Scanner(new File(Path));
+        } catch (FileNotFoundException exception) {
+            System.err.print("ERROR: File not found\n" +
+                    "SYSTEM_MESSAGE: " + exception.getMessage());
+            return;
         }
-        System.out.printf("\nEnter argument to calculate: ");
+
+        System.out.print("\n\tLines function values: ");
+        Path = in.nextLine();
+        try {
+            outLines = new PrintWriter("lines.txt");
+        } catch (FileNotFoundException exception) {
+            System.err.print("ERROR: File not found\n" +
+                    "SYSTEM_MESSAGE: " + exception.getMessage());
+            return;
+        }
+
+        System.out.print("\n\tPolynomial function values: ");
+        Path = in.nextLine();
+        try {
+            outPolynomial = new PrintWriter("polynomial.txt");
+        } catch (FileNotFoundException exception) {
+            System.err.print("ERROR: File not found\n" +
+                    "SYSTEM_MESSAGE: " + exception.getMessage());
+            return;
+        }
+
+        System.out.print("\nInput data processing...\n");
+
+        Integer N = inFile.nextInt();
+        XAndDividedDifference = new HashMap<>(N, 1.0f);
+        Double knownX;
+        for (Integer i = 0; i <= N; i++) {
+            knownX = inFile.nextDouble();
+            XtoY.put(knownX, inFile.nextDouble());
+            XAndDividedDifference.put(knownX, dividedDifference(XtoY));
+        }
+
+        Thread outputDumping = new Thread(()->{
+            System.out.print("\nDumping functions data into output files...\n");
+
+            Double start = XtoY.firstKey(),
+                    end = XtoY.lastKey(),
+                    step = (start - end) / 10;
+            start -= step;
+            end += step;
+            step /= 500;
+            for (Double i = start; i <= end; i+= step) {
+                outLines.printf("%f %f", i, lineFunctionY(XtoY, i));
+                outPolynomial.printf("%f %f", i,
+                        polynomialFunctionY(XAndDividedDifference, i));
+            }
+
+            System.out.print("\nDumping complete.\n");
+        }, "DumpingThread");
+        outputDumping.start();
+
+        System.out.print("\n\nEnter argument to calculate: ");
         Double x = in.nextDouble();
+
+        System.out.printf("\n\nLine result: %f\n",
+                lineFunctionY(XtoY, x));
+        System.out.printf("Polynomial result: %f\n",
+                polynomialFunctionY(XAndDividedDifference, x));
+
+        try {
+            outputDumping.join();
+        } catch (InterruptedException exception) {
+            System.err.print("ERROR: " + outputDumping.getName() + " thread interrupted\n" +
+                    "SYSTEM_MESSAGE: " + exception.getMessage());
+            return;
+        }
+    }
+    private static Double dividedDifference(Map<Double, Double> values) {
+        Set<Double> args = values.keySet();
+        Double prod, sum = 0.0;
+        for (Double i: args) {
+            prod = 1.0;
+            for (Double j: args) {
+                if (!i.equals(j))
+                    prod *= i - j;
+            }
+            sum += values.get(i)/prod;
+        }
+        return sum;
+    }
+    private static Double lineFunctionY(TreeMap<Double, Double> values, Double x) {
         Map.Entry<Double, Double> rPair = null,
                 lPair = null;
         for (Map.Entry<Double, Double> pair:
-             XtoY.entrySet()) {
+                values.entrySet()) {
             if (x < pair.getKey()) {
                 rPair = pair;
                 break;
             }
         }
-        Double y = null;
-        try {
-            lPair = XtoY.lowerEntry(rPair.getKey());
-            y = (x - lPair.getKey()) / (rPair.getKey() - lPair.getKey()) *
-                    (rPair.getValue() - lPair.getValue()) + lPair.getValue();
-        } catch (NullPointerException e){
-            System.out.printf("ERROR: Argument out of range");
-            return;
+        if (rPair == null)
+            rPair = values.lowerEntry(x);
+        lPair = values.lowerEntry(rPair.getKey());
+        if (lPair == null) {
+            lPair = rPair;
+            rPair = values.higherEntry(lPair.getKey());
         }
-        System.out.printf("\nResult: %50.25e", y);
+        return (x - lPair.getKey()) / (rPair.getKey() - lPair.getKey()) *
+                (rPair.getValue() - lPair.getValue()) + lPair.getValue();
     }
-    private static void secondSubtask() {
-        // TODO: add program logic
+    private static Double polynomialFunctionY(Map<Double, Double> values, Double x) {
+        Set<Double> args = values.keySet();
+        Double prod, sum = 0.0;
+        for (Double i: args) {
+            prod = 1.0;
+            for (Double j: args) {
+                if (i.equals(j)) break;
+                prod *= x - j;
+            }
+            sum += values.get(i) * prod;
+        }
+        return sum;
     }
 }
