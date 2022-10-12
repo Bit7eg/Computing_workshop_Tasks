55c95adfe1998c7bdca56afa3ce2793e86f491cf
i.stepanko <i.stepanko@g.nsu.ru>
2022-10-12 14:15:18 +0700

    changes

--- src/IntegralCounter.java
+++ src/IntegralCounter.java
@@ -13,8 +13,38 @@ public class IntegralCounter {
         this.b = maxX;
     }
 
-    public Double calculate() {
-        //TODO: integral calculating
-        return 1.0;
+    public Double leftRectangleCalculate(Integer rectangleNumber) {
+        Double integral = 0.0;
+
+        Double rectangleWidth = (this.b - this.a)/rectangleNumber;
+        for (Double xi = this.a; xi < this.b; xi += rectangleWidth) {
+            integral += function.apply(xi) * rectangleWidth;
+        }
+
+        return integral;
+    }
+
+    public Double centerRectangleCalculate(Integer rectangleNumber) {
+        Double integral = 0.0;
+
+        Double rectangleWidth = (this.b - this.a)/rectangleNumber;
+        for (Double xi = this.a + rectangleWidth/2; xi < this.b; xi += rectangleWidth) {
+            integral += function.apply(xi) * rectangleWidth;
+        }
+
+        return integral;
+    }
+
+    public Double trapezoidCalculate(Integer trapezoidNumber) {
+        Double integral = 0.0;
+
+        Double trapezoidHeight = (this.b - this.a)/trapezoidNumber;
+        Double lastX = this.a;
+        for (Double xi = lastX + trapezoidHeight; xi <= this.b; xi += trapezoidHeight) {
+            integral += (function.apply(lastX) + function.apply(xi))/2 * trapezoidHeight;
+            lastX = xi;
+        }
+
+        return integral;
     }
 }
--- src/Task2.java
+++ src/Task2.java
@@ -1,6 +1,12 @@
 public class Task2 {
-    public static void main(String[] args){
-        Integer cols = 1, rows = 1;
+    public static void main(String[] args) {
+        Integer columnsWidth = 25,
+                firstColumnWidth = 10,
+                numberOutputAccuracy = 20;
+        Integer[] accuracies = {
+                10, 100, 1000,
+        };
+
         IntegralCounter integral = new IntegralCounter(
                 (x)->{
                     return 1.0;
@@ -8,6 +14,23 @@ public class Task2 {
                 0.0,
                 1.0
         );
+        Double realIntegralValue = 1.0;
 
+        System.out.println("Mistake values table:\n");
+        System.out.printf("%" + firstColumnWidth + "s | " +
+                "%" + columnsWidth + "s | " +
+                "%" + columnsWidth + "s | " +
+                "%" + columnsWidth + "s | \n",
+                "", "Left Rectangles", "Center Rectangles", "Trapezoid");
+
+        for (Integer i: accuracies) {
+            System.out.printf("%" + firstColumnWidth + "d | " +
+                            "%" + columnsWidth + "." + numberOutputAccuracy + "f | " +
+                            "%" + columnsWidth + "." + numberOutputAccuracy + "f | " +
+                            "%" + columnsWidth + "." + numberOutputAccuracy + "f | \n",
+                    i, Math.abs(realIntegralValue - integral.leftRectangleCalculate(i)),
+                    Math.abs(realIntegralValue - integral.centerRectangleCalculate(i)),
+                    Math.abs(realIntegralValue - integral.trapezoidCalculate(i)));
+        }
     }
 }
