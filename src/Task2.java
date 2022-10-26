7d76791627f3af79eb0d7ded5ec90d75ec120f58
i.stepanko <i.stepanko@g.nsu.ru>
2022-10-26 14:04:56 +0700

    Function changed. Outputs for Simpson's method and Gauss' method added

--- src/Task2.java
+++ src/Task2.java
@@ -1,36 +1,42 @@
 public class Task2 {
     public static void main(String[] args) {
-        Integer columnsWidth = 25,
+        Integer columnsWidth = 30,
                 firstColumnWidth = 10,
-                numberOutputAccuracy = 20;
+                numberOutputAccuracy = 25;
         Integer[] accuracies = {
                 10, 100, 1000,
         };
 
         IntegralCounter integral = new IntegralCounter(
                 (x)->{
-                    return x*x*x + x*x + x + 1;
+                    return Math.cos(x);
                 },
-                0.0,
-                1.0
+                -Math.PI,
+                Math.PI/2
         );
-        Double realIntegralValue = 2.0 + 1.0/12.0;
+        Double realIntegralValue = 1.0;
 
         System.out.println("Mistake values table:\n");
         System.out.printf("%" + firstColumnWidth + "s | " +
                 "%" + columnsWidth + "s | " +
                 "%" + columnsWidth + "s | " +
+                "%" + columnsWidth + "s | " +
+                "%" + columnsWidth + "s | " +
                 "%" + columnsWidth + "s | \n",
-                "", "Left Rectangles", "Center Rectangles", "Trapezoid");
+                "", "Left Rectangles", "Center Rectangles", "Trapezoid", "Simpson", "Gauss");
 
         for (Integer i: accuracies) {
             System.out.printf("%" + firstColumnWidth + "d | " +
                             "%" + columnsWidth + "." + numberOutputAccuracy + "f | " +
                             "%" + columnsWidth + "." + numberOutputAccuracy + "f | " +
+                            "%" + columnsWidth + "." + numberOutputAccuracy + "f | " +
+                            "%" + columnsWidth + "." + numberOutputAccuracy + "f | " +
                             "%" + columnsWidth + "." + numberOutputAccuracy + "f | \n",
                     i, Math.abs(realIntegralValue - integral.leftRectangleCalculate(i)),
                     Math.abs(realIntegralValue - integral.centerRectangleCalculate(i)),
-                    Math.abs(realIntegralValue - integral.trapezoidCalculate(i)));
+                    Math.abs(realIntegralValue - integral.trapezoidCalculate(i)),
+                    Math.abs(realIntegralValue - integral.SimpsonCalculate(i)),
+                    Math.abs(realIntegralValue - integral.GaussCalculate(i)));
         }
     }
 }
