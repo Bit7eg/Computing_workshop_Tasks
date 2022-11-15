9674a8cc4fa124de02ca8d680158378c38135655
Bit7eg <stonysyrop@gmail.com>
2022-11-15 21:03:54 +0700

    Some changes

--- src/Task3.java
+++ src/Task3.java
@@ -1,5 +1,11 @@
 public class Task3 {
     public static void main(String[] args){
-
+        double a = 1;
+        double b = a/2;
+        while (b != b/2) {
+            a = b;
+            b /= 2;
+        }
+        System.out.println(a);
     }
 }
